bfea3889acfa5deb3ee9bdb869b20c33731b395e
wolfeyuan <wolfe_yuan@163.com>
2018-01-17 13:29:15 +0800

    add ex21 - ex30

---
+++ LearnPythontheHradWay/ex28.py
@@ -0,0 +1,21 @@
+#!/user/bin/python
+#filename:ex28.py
+#-*-coding:utf-8 -*-
+
+print True and True
+print False and True
+
+print 1==1 and 2==1
+print "test" == "test"
+
+print 1==1 or 2!=1
+print True and 1 == 1
+print "test" == "testing"
+print "test"==1
+print "test"!="testing"
+print not(True and False)
+print not(1==1 and 0!=1)
+print not(10==1 or 1000==1000)
+print not(1!=10 or 3==4)
+
+print 3==3 and (not ("testing" == "testing" or "Python" == "Fun"))
